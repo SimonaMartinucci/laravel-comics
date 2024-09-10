7b8978df87ab025fbd4fc1884c5cb4742e78059c
SimonaMartinucci <simo.martinucci3@gmail.com>
2024-09-10 16:25:37 +0200

    feat: add products

--- resources/views/partials/products.blade.php
+++ resources/views/partials/products.blade.php
@@ -1,10 +1,18 @@
+@php 
+
+$products = config('products');
+
+@endphp
+
 <div class="content-cont">
     <div class="small_container">
+        @foreach($products as $product)
         <div class="card">
             <div class="card-img">
-                <img src="#" :alt="#">
+                <img src="{{ $product['thumb'] }}" alt="{{ $product['series'] }}">
             </div>
-            <h4>Nome</h4>
+            <h4>{{ $product['series'] }}</h4>
         </div>
+        @endforeach
     </div>
 </div>
