1d30e789fdc36032bcf00e7a591a880c760d08c1
SimonaMartinucci <simo.martinucci3@gmail.com>
2024-09-10 16:47:18 +0200

    feat: add dynamic merch menu

---
+++ config/menus/merch.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    [ 
+        "name"=> "DIGITAL COMICS", 
+        "img"=> "buy-comics-digital-comics.png" 
+    ],
+    [ 
+        "name"=> "DC MERCHANDISE", 
+        "img"=> "buy-comics-merchandise.png" 
+    ],
+    [ 
+        "name"=> "SUBSCRIPTION", 
+        "img"=> "buy-comics-subscriptions.png" 
+    ],
+    [ 
+        "name"=> "COMIC SHOP LOCATION", 
+        "img"=> "buy-comics-shop-locator.png" 
+    ],
+    [ 
+        "name"=> "DC POWER VISA", 
+        "img"=> "buy-dc-power-visa.svg" 
+    ],
+];
--- resources/views/partials/merch.blade.php
+++ resources/views/partials/merch.blade.php
@@ -1,10 +1,18 @@
+@php
+
+    $merch = config('menus.merch');
+
+@endphp
+
 <div class="merch-cont">
         <div class="container">
+            @foreach($merch as $item)
            <div class="item">
                 <div class="item_img">
-                    <img src='#' alt="#">
+                <img src="{{ Vite::asset('resources/img/' . $item['img']) }}" alt="{{ $item['name'] }}">
                 </div>
-                <div class="item_descr">Nome</div>
+                <div class="item_descr">{{ $item['name'] }}</div>
            </div>
+           @endforeach
         </div>
     </div>
